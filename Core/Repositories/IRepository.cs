8a47d9e008d5afcd670fba3cadece4280b278c7c
Emreman5 <emre15087@gmail.com>
2022-11-05 00:14:05 +0300

    Repository Pattern implementation

---
+++ Core/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Entities.Abstract;
+
+namespace Core.Repositories;
+
+public interface IRepository<T, in TKey> where T : class, IDatabaseEntity<TKey>, new() where TKey : IEquatable<TKey>
+{
+    T? Get(Expression<Func<T, bool>> predicate);
+    T Add(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+  
+}
