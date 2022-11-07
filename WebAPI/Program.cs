182ce6ae24e4120614a9615d0f925cad5d25345e
Emreman5 <emre15087@gmail.com>
2022-11-07 11:31:42 +0300

    configuration

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,19 @@
+using DataAccess.Concrete.Context;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var conf = builder.Configuration;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MsSqlDbContext>(optionsBuilder =>
+    optionsBuilder.UseSqlServer(conf.GetConnectionString("MsSqlConnectionString")));
 
 var app = builder.Build();
 
