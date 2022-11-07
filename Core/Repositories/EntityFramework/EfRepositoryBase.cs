bd4c5ed8988206b0e7edb90b2a34f92c269b7770
Emreman5 <emre15087@gmail.com>
2022-11-07 11:30:46 +0300

    typo

--- Core/Repositories/EntityFramework/EfRepositoryBase.cs
+++ Core/Repositories/EntityFramework/EfRepositoryBase.cs
@@ -1,78 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
+﻿using System.Linq.Expressions;
 using Core.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Core.Repositories.EntityFramework;
 
 public abstract class EfRepositoryBase<TEntity, TContext> : IUnitOfWork ,IRepository<TEntity, int> where TEntity : DatabaseEntity, new()
 
 {
-    private readonly DbContext Context;
+    private readonly DbContext _context;
 
     protected EfRepositoryBase(DbContext context)
     {
-        Context = context;
+        _context = context;
     }
     public async Task<DatabaseEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate)
     {
-        return await Context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+        return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
     }
     public async Task<TEntity> AddAsync(TEntity entity)
     {
-        Context.Entry(entity).State = EntityState.Added;
-        await Context.SaveChangesAsync();
+        _context.Entry(entity).State = EntityState.Added;
+        await _context.SaveChangesAsync();
         return entity;
     }
     public async Task<TEntity> UpdateAsync(TEntity entity)
     {
-        Context.Entry(entity).State = EntityState.Modified;
-        await Context.SaveChangesAsync();
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
         return entity;
     }
     public async Task<TEntity> DeleteAsync(TEntity entity)
     {
-        Context.Entry(entity).State = EntityState.Deleted;
-        await Context.SaveChangesAsync();
+        _context.Entry(entity).State = EntityState.Deleted;
+        await _context.SaveChangesAsync();
         return entity;
     }
     public TEntity? Get(Expression<Func<TEntity, bool>> predicate)
     {
-        return Context.Set<TEntity>().FirstOrDefault(predicate);
+        return _context.Set<TEntity>().FirstOrDefault(predicate);
     }
     public TEntity Add(TEntity entity)
     {
-        Context.Entry(entity).State = EntityState.Added;
+        _context.Entry(entity).State = EntityState.Added;
         return entity;
     }
     public TEntity Update(TEntity entity)
     {
-        Context.Entry(entity).State = EntityState.Modified;
+        _context.Entry(entity).State = EntityState.Modified;
         return entity;
     }
     public TEntity Delete(TEntity entity)
     {
-        Context.Entry(entity).State = EntityState.Deleted;
+        _context.Entry(entity).State = EntityState.Deleted;
         return entity;
     }
 
     public void Dispose()
     {
-        Context.Dispose();
+        _context.Dispose();
         GC.SuppressFinalize(this);
     }
 
     public int Commit()
     {
-        return Context.SaveChanges();
+        return _context.SaveChanges();
     }
 
     public async Task<int> CommitAsync()
     {
-        return await Context.SaveChangesAsync();
+        return await _context.SaveChangesAsync();
     }
+    
+    // 
 }
